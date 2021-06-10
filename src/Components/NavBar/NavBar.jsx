696b5f2758eac10272e5553b4c8f9d14e9e0a990
Fedecolombo1 <colombofederico17@gmail.com>
2021-06-10 00:55:32 -0300

    Probando hacer navbar y banner principal

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -8,10 +8,10 @@ function NavBar() {
 
   return (
     <>
-      <nav className="navbar col-12 desk">
-        <a className="col-2" href="https://fedecolombo1.github.io/Home/"><h1 class="col-12 nombre">-LOGO</h1></a>
+      <header className="navbar col-12 desk">
+        <a className="col-2" href="https://fedecolombo1.github.io/Home/"><h1 className="col-12 nombre">-LOGO</h1></a>
         <div className="col-6 row align">
-            <Dropdown menuAlign="center" className="col-2 align links">
+            <Dropdown menualign="center" className="col-2 align links">
               <Dropdown.Toggle variant="success" id="dropdown-basic">
                 Cursos <FontAwesomeIcon style={{marginLeft:'5px',fontSize:'15px'}} icon={faChevronDown} />
               </Dropdown.Toggle>
@@ -22,7 +22,7 @@ function NavBar() {
                 <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-            <Dropdown menuAlign="center" className="col-2 align links">
+            <Dropdown menualign="center" className="col-2 align links">
               <Dropdown.Toggle variant="success" id="dropdown-basic">
                 Recursos <FontAwesomeIcon style={{marginLeft:'5px',fontSize:'15px'}} icon={faChevronDown} />
               </Dropdown.Toggle>
@@ -37,12 +37,12 @@ function NavBar() {
             <h2 className="col-2 align links"><a className="navLinks" href="#estudios1"> Nosotros</a></h2>
             <h2 className="col-2 align links"><a className="navLinks" href="#estudios1"> Contacto</a></h2>
         </div>
-        <a class="col-2" style={{justifyContent: 'flex-end', display:'flex', textDecoration:'none'}} href="https://fedecolombo1.github.io/Home/"><button style={{marginRight:'6%'}} className="col-6 botonC">Ver Cursos</button></a>
+        <a className="col-2" style={{justifyContent: 'flex-end', display:'flex', textDecoration:'none'}} href="https://fedecolombo1.github.io/Home/"><button style={{marginRight:'6%'}} className="col-6 botonC">Ver Cursos</button></a>
         
-      </nav>
+      </header>
 
-      <nav className="row col-12 align mobile">
-        <navbar className="col-6 row align navbar">
+      <header className="row col-12 align mobile">
+        <nav className="col-6 row align navbar">
           <div className="menu-mobile col-12">
             <button id="bt-menu-mobile">
               <span className="top"></span>
@@ -51,12 +51,12 @@ function NavBar() {
             </button>
             <div className="clear"></div>
           </div>
-        </navbar>
+        </nav>
 
         <a className="col-6 row align logo" href="https://fedecolombo1.github.io/Home/">
           <h1 className="col-12 nombre">-LOGO</h1>
         </a>
-      </nav>
+      </header>
     </>
   );
 
