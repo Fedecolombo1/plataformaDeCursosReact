45e521edf7dd96e6a565ffc3b62c47d1b8242f11
Fedecolombo1 <colombofederico17@gmail.com>
2021-06-10 17:16:32 -0300

    changing text banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ function Banner() {
         <div className="col-12 row banner desk align">
             <div className='col-12 col-lg-6 row'>
               <h1 className='Titulo col-12'> Espacio de formacion</h1>
-              <h3 className='subTitulo col-12'> Orientado a profesionales de la salud</h3>
+              <h3 className='subTitulo2 col-12'> Orientado a <span className='dos'></span></h3>
             </div>
             <img src={Foto} alt='gif' className="col-12 col-lg-3 foto" />
         </div>
@@ -19,7 +19,7 @@ function Banner() {
             </div>
             <div className='col-12 row'>
               <h1 className='Titulo col-12'> Espacio de formacion</h1>
-              <h3 className='subTitulo col-12'> Orientado a profesionales de la salud</h3>
+              <h3 className='subTitulo1 col-12'> Orientado a <span className='uno'></span></h3>
             </div>
             <a className="col-12" style={{justifyContent: 'flex-start', display:'flex', textDecoration:'none'}} href="https://fedecolombo1.github.io/Home/">
               <button  className="col-4 botonC">Saber Mas</button>
