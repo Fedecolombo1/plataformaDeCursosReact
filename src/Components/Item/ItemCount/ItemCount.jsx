fad6de2716a0aa7b84ea9f357f24611bed6b8a83
Fedecolombo1 <colombofederico17@gmail.com>
2021-06-16 00:25:24 -0300

    terminada la logica de contador

--- src/Components/Item/ItemCount/ItemCount.jsx
+++ src/Components/Item/ItemCount/ItemCount.jsx
@@ -1,30 +1,35 @@
+import { React, useState } from 'react';
 import './ItemCount.css'
 
 
-const ItemCount = () => {
+const ItemCount = (props) => {
 
-    /*let contador = 0
+    const [Contador, setContador] = useState(props.initial)
 
-    let menos = document.getElementById('menos')
-
-    console.log(document.getElementById('menos') + 'HOLAAAA');
-
-    let mas = document.getElementById('mas')
-
-    let cantidadReal = document.getElementById('cantidadReal')
-
-    menos.addEventListener(onclick, () => {
-        return contador + 1
-    })*/
+    const operar = (operador) => {
+        if(operador === '+'){
+            if(Contador < props.stock){
+                setContador(Contador + 1)
+            } else{
+                setContador(props.stock)
+            }
+        } else if(operador === '-'){
+            if(Contador === 1){
+                setContador(1)
+            } else {
+                setContador(Contador - 1)
+            }
+        }
+    }
 
     return(
         <div className='col-12 col-lg-3 align row'>
             <div className="cantidad col-10 col-lg-12 align">
-                <button id='menos' className='masMenos align'>-</button>
-                <span id='cantidadReal' className='cantidadReal'></span>
-                <button id='mas' className='masMenos align'>+</button>
+                <button id='menos' className='masMenos align' onClick={() => operar('-')}>-</button>
+                <input type='text' className='valor' value={Contador} />
+                <button id='mas' className='masMenos align' onClick={() => operar('+')} >+</button>
             </div>
-            <button className="agregar col-10 col-lg-12">Agregar al carrito</button>
+            <button className="agregar col-10 col-lg-12" onClick={() => { if(props.stock >= 1){setContador('Hay Stock y agregaste ' + {Contador}.Contador + ' :)')} else {setContador('No hay stock :(')} }}>Agregar al carrito</button>
         </div>
     )
 }
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -4,12 +4,14 @@ import ItemCount from '../Item/ItemCount/ItemCount'
 
 const ItemListContainer = ({greeting}) => {
 
+    
+
     return(
         <>
             <span className='col-10 tituloSecSpan'><h1 className='col-12 align TituloSec'>{greeting}</h1></span>
             <ProductCard />
             <div className="col-12 align">
-                <ItemCount stock='10'/>
+                <ItemCount initial={1} stock={5} />
             </div>
         </>
     )
